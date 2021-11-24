0008c5fe955e67d8fbc275c7ae4a8a468e92d5cd
shenyifan <605729908@qq.com>
2021-11-24 11:22:07 +0800

    upup

--- web/src/views/home/index.vue
+++ web/src/views/home/index.vue
@@ -170,6 +170,7 @@ export default {
             needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
             scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
             success: function(res) {
+              console.log(res.resultStr)
               var result = res.resultStr
               alert(result)
             }
