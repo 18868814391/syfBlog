709a0b0b64786ca50ef16c21d5df2f54624f322f
shenyifan <605729908@qq.com>
2021-11-24 11:17:36 +0800

    upup

--- web/src/views/home/index.vue
+++ web/src/views/home/index.vue
@@ -167,10 +167,11 @@ export default {
       return new Promise((resolve, reject) => {
         wx.ready(function() {
           wx.scanQRCode({
-            needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+            needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
             scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
             success: function(res) {
-
+              var result = res.resultStr
+              alert(result)
             }
           })
         })
